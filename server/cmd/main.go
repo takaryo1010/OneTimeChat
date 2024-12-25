c261f56ad040448cad64e7ffa1fa24d0465adcf4
takaryo1010 <t.ryota1010@gmail.com>
2024-12-25 11:00:48 +0000

    change:main

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -7,17 +7,5 @@ import (
 )
 
 func main() {
-	// ルートディレクトリ
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
 
-	// WebSocket接続
-	http.HandleFunc("/ws", handleConnections)
-	go handleMessages()
-
-	fmt.Println("Chat server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
 }
